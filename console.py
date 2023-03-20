be381f036691e7b253f5c57556291f1bd8914914
Sergey <pristup2040@gmail.com>
2023-03-20 15:00:10 +1000

    first commit console.py - finished

---
+++ console.py
@@ -0,0 +1,132 @@
+import curses
+
+
+class Console:
+    def __init__(self, width=150, height=49):
+        self.stdscr = curses.initscr()
+        self.max_width = width # ширина терминала
+        self.max_height = height # высота терминала
+
+        curses.curs_set(0)
+        curses.noecho()
+        curses.cbreak()
+        self.stdscr.keypad(True)
+
+        # получаем высоту и ширину терманала пользователя
+        current_console_height, current_console_width = self.stdscr.getmaxyx()
+
+        if current_console_width < width:
+            raise Exception(f'Ширина консоли меньше {width}')
+
+        if current_console_height < height:
+            raise Exception(f'Ширина консоли меньше {height}')
+
+    def finish_work(self):
+        curses.nocbreak()
+        self.stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+
+    def clear(self):
+        # Очищает консоль
+        self.stdscr.clear()
+
+    # def refresh(self):
+    #     # Обновляет консоль, вносит все сделанные изменения
+    #     self.stdscr.refresh()
+
+    def get_char(self):
+        # Возвращает ASCII-код нажатой клавиши
+        key = self.stdscr.getch()
+        return key
+
+    def write_line(self, x: int, y: int, line: str):
+        # Вывод строки в консоль
+        if len(line)+x > self.max_width:
+            line = line[:(self.max_width-x)]
+        if len(line) == 0:
+            return
+        if x >= self.max_width or y >= self.max_height:
+            return
+
+        self.stdscr.addstr(y, x, line)
+
+    def write_column(self, x: int, y: int, column: str):
+        # выводит колонку
+        if len(column) > self.max_height:
+            column = column[:(self.max_height-y)]
+        if len(column) == 0:
+            return
+        if x >= self.max_width or y >= self.max_height:
+            return
+
+        for i in range(len(column)):
+            self.stdscr.addstr(y, x, column[i])
+            y+=1
+
+    def write_object_by_list_of_string(self, x: int, y: int, list_of_string: list):
+        # выводит список со строками
+        for i in range(len(list_of_string)):
+            self.write_line(x, y, list_of_string[i])
+            y += 1
+
+    def write_object_by_long_string(self, x: int, y: int, big_string: str):
+        # выводит длинноу строку с переносами строк
+        list_of_string = big_string.split('\n')
+        for i in range(len(list_of_string)):
+            self.write_line(x, y, list_of_string[i])
+            y += 1
+
+    def write_border(self, x: int, y: int, width: int, height: int):
+        # рисует рамку
+        self.write_line(x, y, f'┌{"─" * (width-2)}┐')
+        self.write_line(x, y+height-1, f'└{"─" * (width-2)}┘')
+        self.write_column(x, y+1, '│'*(height-2))
+        self.write_column(x+width-1, y+1, '│'*(height-2))
+
+    def del_border(self, x: int, y: int, width: int, height: int):
+        # удаляет рамку
+        self.write_line(x, y, width * ' ')
+        self.write_line(x, y+height-1, width * ' ')
+        self.write_column(x, y+1, ' '*(height-2))
+        self.write_column(x+width-1, y+1, ' '*(height-2))
+
+
+if __name__ == '__main__':
+    console1 = Console()
+    a = [
+    [
+        '-----',
+        '-0-0-',
+        '-----',
+        '-----',
+    ],
+    [
+        '-----',
+        '-X-X-',
+        '-----',
+        '-----',
+    ],
+     ]
+
+    for i in range(2):
+        if i == 0:
+            console1.write_border(1, 1, 9, 8)
+        else:
+            console1.del_border(1, 1, 9, 8)
+
+        console1.write_object_by_list_of_string(3, 3, a[i])
+        console1.get_char()
+
+    console1.clear()
+    console1.write_line(30, 30, 'Вы погибли!')
+    console1.get_char()
+
+    # while True:
+    #     console1.write_line(5, i, '@'*4*(i+1))
+    #
+    #     s = console1.get_char()
+    #     i+=1
+
+
+
